22dc9f22fd8648f924645479b210b62573daa20c
Rafael Velásquez Millán <rafael_v56@hotmail.com>
2019-05-19 19:12:04 +0200

    Creado el proyecto vacío
    Se creo el proyecto para iniciar la actividad en git
    
    Se creo un proyecto java vacío para empezar a crear
    los ejemplos de lo diferentes temas de la página web
    
    Signed-off-by: Rafael Velásquez Millán <rafael_v56@hotmail.com>

---
+++ src/ejemplosdecodig/princiapal/Principal.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package ejemplosdecodig.princiapal;
+
+/**
+ * @author ejemplos de codigo
+ *
+ */
+public class Principal {
+
+	/**
+	 * Método principal
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("hola mundo");
+	}
+
+}
